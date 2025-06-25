83297ed009172173862aa4926e12e7d6ac7eb980
Error1234543 <ptaviyad21@gmail.com>
2025-06-25 11:31:51 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,12 +3,12 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "25933223"))
-API_HASH = environ.get("API_HASH", "6ef5a426d85b7f01562a41e6416791d3")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7373062125:AAFJY69INZUEF3tcnQ4ORohcOxb3zi4QMUA")
-OWNER = int(environ.get("OWNER", "8118667253"))
+API_ID = int(environ.get("API_ID", "20619533"))
+API_HASH = environ.get("API_HASH", "5893568858a096b7373c1970ba05e296")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8146069942:AAFjlkaOVGnw1iGdV3U1nJ72pOBC2e2Bg_w")
+OWNER = int(environ.get("OWNER", "7447651332"))
 CREDIT = "S͜͡u͜͡j͜͡a͜͡l͜͡k͜͡i͜͡n͜͡g͜͡"
-AUTH_USER = os.environ.get('AUTH_USERS', '8118667253').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '7447651332').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
